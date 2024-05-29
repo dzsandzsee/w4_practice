e664fa30e54f34dedf36abf5f5d11b5ec44457ec
dzsandzsee <dzsandzsee@gmail.com>
2024-05-29 18:08:01 +0200

    added hello world functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function helloWorld () {
+    console.log("hello world")
+}
+
+helloWorld()
+
+const coolerHelloWorld = function() {
+    console.log("hello world from the cooler functions")
+}
+
+
+coolerHelloWorld()
+
