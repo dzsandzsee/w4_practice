40c7eba7bb51a7880e5fc972496390d48e54b36b
dzsandzsee <dzsandzsee@gmail.com>
2024-06-03 20:03:26 +0200

    did cb methotds

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-/* function helloWorld () {
-    console.log("hello world")
-}
+const data = ["one", "two", "three", "four", "five", "six", "seven"]
+// DO NOT HAVE RETURN VALUE ---> undifined
+/* data.forEach((element, index, array) => {
+    console.log(`The ${index}. element of the "${array}" array is: ${element}`)
+}) */
+/* data.forEach((element) => {
+    console.log(element)
+}) */
+// MAP ALWAYS HAS RETURN VALUE -----> ARRAY WITH SAME LENGHTH AS ORIGINAL 
+/* data.map((element) => {
+    return `new${element}`
 
-helloWorld()
-
-const coolerHelloWorld = function() {
-    console.log("hello world from the cooler functions")
-}
-
-
-coolerHelloWorld()
-
-const coolestHelloWorld = () => {
-    console.log("hello world from the arrow function")
-}
-coolestHelloWorld() */
-
-function sumOfTwoNumbers (number1, number2) {
-    return number1 + number2
-}
-function multiplyTwoNumbers(number1, number2) {
-    return number1 + 10
-}
-/* const result = sumOfTwoNumbers(1, 2)
+})
 console.log(result) */
 
-//console.log(sumOfTwoNumbers(20, 3));
+//FILTER
 
-/* const arrowSumOfTwoNumbers = (number1, number2) => number1 + number2
+/* const result = data.filter((element) => element.length > 3)
+console.log(result) */
+
+//REDUCE 
+/* 1. prev értéke "jéghidegmitiszol"
+curr : one
+Cb: prev+ curr = jeghidegmitiszol + one 
+
+2. prev értéke "jéghidegmitiszolone"
+curr : two
+cb: prev + curr = jeghidegmitiszolone + two
+*/
+//console.log(result)
+//const result = data.reduce((prev, curr) => prev + curr, "jeghidegmitiszol")
+
+/*
+ha nincs initialvalue
+1. prev = első elem = "one"
+current a 2. eleme = "two"
 
 
 
-console.log(arrowSumOfTwoNumbers(1, 4)) */
 
-function doSomeMagic(number1, number2, callback) {
-    return callback(number1, number2)
-}
-console.log(doSomeMagic(1, 2, multiplyTwoNumbers))
+
+
+*/
+const result = data.find(element => element.length > 3)
+console.log(result)
